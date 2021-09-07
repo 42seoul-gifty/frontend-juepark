0ad284dae63b09e8a41224462473b4f1147aa06b
humonnom <josojoso0702@gmail.com>
2021-09-07 12:03:35 +0900

    [rebuild]

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import { PageWrapper } from '../components';
-import { BasicButton } from '../components';
+import React from "react";
+import { PageWrapper } from "../components";
+import { BasicButton } from "../components";
 
 function Main(props) {
+  // check if user logged in
+  // const login = true;
 
-	// check if user logged in
-	// const login = true;
+  // if (login === true) {
+  // 	window.location.assign("/gift")
+  // } else {
+  // 	window.location.assign("/login")
+  // }
 
-	// if (login === true) {
-	// 	window.location.assign("/gift")
-	// } else {
-	// 	window.location.assign("/login")
-	// }
-
-	return (
-		<PageWrapper>
-			yourwish
-			<BasicButton do={() => window.location.assign("/login")} text="로그인"/>
-			<BasicButton do={() => window.location.assign("/gift")} text="선물하기"/>
-		</PageWrapper>
-	);
+  return (
+    <PageWrapper>
+      yourwish
+      <div style={{
+		  display:"flex",
+		  flexDirection:"row"
+	  }}>
+        <BasicButton
+          do={() => window.location.assign("/login")}
+          text='로그인'
+        />
+        <BasicButton
+          do={() => window.location.assign("/gift")}
+          text='선물하기'
+        />
+      </div>
+    </PageWrapper>
+  );
 }
 
 export default Main;
