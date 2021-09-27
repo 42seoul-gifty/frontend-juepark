20accef8332cb78bd05c4403db0f2b061501b73f
humonnom <josojoso0702@gmail.com>
2021-09-27 17:36:15 +0900

    feat: edit index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,16 +4,16 @@ import GiftFooter from "./footer/GiftFooter";
 import BasicButton from "./button/BasicButton";
 import GiftBody from "./body/GiftBody";
 import GiftBodyConfirm from "./body/GiftBodyConfirm";
-import GiftBodyFinish from "./body/GiftBodyFinish"
-import GiftBodyInfoGift from "./body/GiftBodyInfoGift"
-import GiftBodyInfoReceiver from "./body/GiftBodyInfoReceiver"
-import GiftBodyInfoSender from "./body/GiftBodyInfoSender"
-import BrandLogo from "./body_element/BrandLogo"
-import GiftImage from "./body_element/GiftImage"
-import Hamburger from "./body_element/Hamburger"
-import Message from "./body_element/Message"
-import Desc from "./body_element/Desc"
+import GiftBodyFinish from "./body/GiftBodyFinish";
+import GiftBodyInfoGift from "./body/GiftBodyInfoGift";
+import GiftBodyInfoReceiver from "./body/GiftBodyInfoReceiver";
+import GiftBodyInfoSender from "./body/GiftBodyInfoSender";
+import GiftImage from "./body_element/GiftImage";
+import Hamburger from "./body_element/Hamburger";
+import Message from "./body_element/Message";
+import Desc from "./body_element/Desc";
 import Form from "./body_element/Form";
+import BrandLogo from "./body_element/BrandLogo";
 
 export {
 	PageWrapper,
