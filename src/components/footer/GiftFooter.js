63555676c7bba1621b1156d4cafe25ad01d528e2
humonnom <josojoso0702@gmail.com>
2021-09-30 18:09:00 +0900

    feat: add json-server setting

--- src/components/footer/GiftFooter.js
+++ src/components/footer/GiftFooter.js
@@ -2,30 +2,24 @@ import React from "react";
 import { BasicButton } from "..";
 import { sendHome, sendGiftMain } from "../../utils/utils";
 
-function GiftFooter({
-  gift_age,
-  gift_price,
-  gift_gender,
-  pageId,
-  onIncrease,
-  onDecrease,
-}) {
+function GiftFooter({ gift_age, gift_price, gift_gender, pageId, onIncrease, onDecrease }) {
   let footerInfo = {
     before: undefined,
     next: undefined,
     beforeText: "",
     nextText: "",
   };
+  let giftList = [];
 
   if (pageId === 0) {
     footerInfo.before = sendHome;
     footerInfo.nextText = "시작하기";
   }
   if (pageId === 4) {
-    // new URLSearchParams(this.props.location.search).get("__firebase_request_key")
-    footerInfo.next = async function getData() {
+    footerInfo.next =
+      async function getData() {
       const fetchedData = await fetch(
-        "http://82f8-125-133-83-39.ngrok.io/products",
+        "http://localhost:3000/products",
         {
           method: "GET",
           // headers: {
@@ -47,7 +41,9 @@ function GiftFooter({
           return data;
         });
 
-      console.log(fetchedData);
+        giftList = fetchedData;
+        console.log(giftList);
+        onIncrease();
     };
   }
   if (pageId === 5) {
