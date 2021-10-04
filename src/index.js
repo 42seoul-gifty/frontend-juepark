216c6ddb985e25fa68bb8ea36850896e7687c367
humonnom <josojoso0702@gmail.com>
2021-10-04 23:29:54 +0900

    feat: add .env

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
 import { store } from "./redux/slice";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
